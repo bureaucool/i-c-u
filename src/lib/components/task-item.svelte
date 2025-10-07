31302b060fe3eae46247adf2e5eda20cee1ad463
Ben <service@bureau.cool>
2025-10-07 23:45:31 +0200

    task item stlting

--- src/lib/components/task-item.svelte
+++ src/lib/components/task-item.svelte
@@ -8,24 +8,26 @@
 	}: { task: Task; clickComplete: () => void; clickEdit: () => void } = $props();
 </script>
 
-<div class="flex flex-row gap-x-2">
+<div class="flex flex-row items-center gap-x-1">
 	<button
 		aria-label="Complete task"
-		class="flex h-8 w-8 items-center justify-center rounded-full border-2 border-black"
+		class="mb-2 flex h-5 w-5 items-center justify-center rounded-full border-2 border-black"
 		onclick={clickComplete}>&nbsp;</button
 	>
-	<button class="flex flex-col gap-y-2" onclick={clickEdit}>
-		<div>
+	<button class="flex flex-col items-start gap-y-0" onclick={clickEdit}>
+		<div class="text-2xl leading-none">
 			<span>{task.emoji}</span>
 			<span>{task.title}</span>
 		</div>
-		<div>
-			{#if task.scheduledAt}
-				<span>{new Date(task.scheduledAt).toLocaleDateString()}</span>
-			{/if}
-			{#if task.assignedUserId}
-				<span>{task.assignedUserId}</span>
-			{/if}
+		<div class="">
+			<div class="pl-1">
+				{#if task.scheduledAt}
+					<span>{new Date(task.scheduledAt).toLocaleDateString()}</span>
+				{/if}
+				{#if task.assignedUserId}
+					<span>{task.assignedUserId}</span>
+				{/if}
+			</div>
 		</div>
 	</button>
 </div>
